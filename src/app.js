2e052acf1cd7be0da1fd9bf320b88b4aa6773882
RicardoContreira <ricardocontreira23@gmail.com>
2020-06-25 18:06:41 -0300

    Primeiro desafio realizado com nodejs. first project with nodejs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,70 @@ app.use(cors());
 
 const repositories = [];
 
+function validateId(request, response, next) {
+  const { id } = request.params
+
+  const existsID = repositories.find(repo => repo.id === id)
+
+  if (!existsID) {
+    return response.status(400).json({ error: 'Repository not found' })
+  }
+
+  return next()
+
+}
+
+app.use('/repositories/:id', validateId)
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(repository)
+
+  return response.json(repository)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repository = repositories.find(repo => repo.id === id)
+
+  repository.title = title
+  repository.url = url
+  repository.techs = techs
+
+  return response.json(repository)
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send()
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repository = repositories.find(repo => repo.id === id)
+
+  repository.likes += 1
+
+  return response.json(repository)
+
 });
 
 module.exports = app;
